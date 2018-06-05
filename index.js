629d755e24ecbd46018429cd7c0c935e79279376
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-05 13:44:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '0fdff22a80a9729dc7e5c1028601c077982ff4c1';
+fetch('https://api.github.com/user/repos', {
+  headers: {Authorization: `token ${token}`}
+})
+.then(res => res.json())
+.then(json => console.log(json));
